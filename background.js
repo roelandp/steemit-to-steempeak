857fc408b84719fb4e3e5e288a308969a880edeb
roelandp <248064+roelandp@users.noreply.github.com>
2020-03-09 16:06:42 +0800

    happy browsing

---
+++ background.js
@@ -0,0 +1,14 @@
+var host = "http://steempeak.com";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://steemit.com/*",
+            "*://www.steemit.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
